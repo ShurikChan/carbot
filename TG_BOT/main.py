6a5a2494d31437b82eeae9c88fd3273e33078312
ShurikChan <pahan_techies@list.ru>
2024-10-10 02:11:24 +0300

    registration updated

--- TG_BOT/main.py
+++ TG_BOT/main.py
@@ -1,9 +1,9 @@
 import telebot
 from telebot import types
 import requests
+from config import *
 
-BOT_TOKEN = '7043798224:AAHZrDLpywajeS6TzJZdr767Lba9CHabHko'
-API_URL_REG = 'http://localhost:8000/register_user/'
+
 bot = telebot.TeleBot(BOT_TOKEN, parse_mode=None)
 
 
@@ -13,17 +13,27 @@ def send_welcome(message):
     button = types.InlineKeyboardButton(
         text="Register", callback_data="register")
     keyboard.add(button)
-    bot.reply_to(message, "Howdy, how are you doing?", reply_markup=keyboard)
+    bot.reply_to(message, "Howdy, how are you doing?",
+                 reply_markup=keyboard)
 
 
 @bot.callback_query_handler(func=lambda call: call.data == "register")
 def register_user(call):
     user_id = call.from_user.id
-    data = {"username": user_id, "password": user_id}  # Имя = Паролю
-    requests.post(API_URL_REG, json=data)
-    bot.edit_message_text(chat_id=call.message.chat.id,
-                          message_id=call.message.message_id, text="Аккаунт создан")
+    response = requests.get(API_URL_REG)
+    users = response.json()
+    filtered_users = filter(
+        lambda user: user['username'] == str(user_id), users)
+    found_users = list(filtered_users)
+    if found_users:
+        bot.edit_message_text(chat_id=call.message.chat.id,
+                              message_id=call.message.message_id, text="Такой пользователь уже зарегистрирован")
+    else:
+        data = {"username": user_id, "password": user_id}  # Имя = Паролю
+        requests.post(API_URL_REG, json=data)
+        bot.edit_message_text(chat_id=call.message.chat.id,
+                              message_id=call.message.message_id, text="Аккаунт создан")
 
 
 if __name__ == '__main__':
-    bot.polling()
+    bot.polling(none_stop=True)
